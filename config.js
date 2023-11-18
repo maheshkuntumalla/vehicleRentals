dfaa05ecfd7a85889bf881a1d0eb37838031122a
Mahesh K <maheshkuntumalla2001@gmail.com>
2023-11-18 17:21:45 +0530

    update on vehicleBooking API

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 require('dotenv').config()
 
-//================================this for locally running purpose==================================================
-
 var config = {
     server: process.env.DB_SERVER, // eg:: 'DESKTOP_mjsi\\MSSQLEXPRESS'
     user: process.env.DB_USER,      // please read above note
@@ -16,19 +14,6 @@ var config = {
 }
 
 
-//================================this for Dockerized running purpose==================================================
-// var config = {
-//     server: 'db', // eg:: 'DESKTOP_mjsi\\MSSQLEXPRESS'
-//     user: 'sa',      // please read above note
-//     password: 'mahe@123',   // please read above note
-//     options: {
-//         trustedconnection: true,
-//         enableArithAort: true,
-//         instancename: 'WIN-RVBJS380SPR\MSSQLSERVER1',
-//         trustServerCertificate: true
-//     },
-//     port: 1433
-// }
 
 
 module.exports = config      
--- controllers/addVehicle.controller.js
+++ controllers/addVehicle.controller.js
@@ -55,70 +55,79 @@ router.post('/vehicleModel', async (req, res) => {
 // API for vehicle Booking
 router.post('/vehicleBooking', async (req, res) => {
 
-    let { user_first, user_last, vehicle_id, start_date, end_date } = req.body
+    try {
+        let { user_first, user_last, vehicle_id, start_date, end_date } = req.body
 
-    const pool = await sql.connect(config)
+        const pool = await sql.connect(config)
 
-    if (user_first === null || user_first === undefined, user_first.trim() === "") {
-        res.status(400).send({ error: true, message: "User first name is required" })
-        return
-    }
+        if (user_first === null || user_first === undefined, user_first.trim() === "") {
+            res.status(400).send({ error: true, message: "User first name is required" })
+            return
+        }
 
-    if (user_last === null || user_last === undefined, user_last.trim() === "") {
-        res.status(400).send({ error: true, message: "User first name is required" })
-        return
-    }
+        if (user_last === null || user_last === undefined, user_last.trim() === "") {
+            res.status(400).send({ error: true, message: "User first name is required" })
+            return
+        }
 
-    if (vehicle_id === null || vehicle_id === undefined) {
-        res.status(400).send({ error: true, message: "vehicle model is required" })
-        return
-    }
+        if (vehicle_id === null || vehicle_id === undefined) {
+            res.status(400).send({ error: true, message: "vehicle model is required" })
+            return
+        }
 
-    let checkOverlap = await pool.request()
-        .input('vehicle_id', sql.Int, vehicle_id)
-        .input('newStartDate', sql.DateTime, start_date)
-        .input('newEndDate', sql.DateTime, end_date)
-        .query(`
-                SELECT *
-                FROM Rentals
-                WHERE (
-                        (r_start_date <= @newEndDate AND r_end_date >= @newStartDate) OR
-                        (r_start_date <= @newStartDate AND r_end_date >= @newEndDate) OR
-                        (r_start_date >= @newStartDate AND r_end_date <= @newEndDate)
-                    ) AND r_vehicle_id =  @vehicle_id;
-                `)
-
-
-    if(checkOverlap.rowsAffected[0]){
-        res.status(409).send({error: false, message: 'Bookings exist for this vehicle at the selected time. Choose another.'})
-        return
-    }
-
-    let addUser = await pool.request()
-        .input('user_first', sql.NVarChar, user_first)
-        .input('user_last', sql.NVarChar, user_last)
-        .query(`
-                        INSERT INTO Users(u_first_name, u_last_name)
-                        VALUES (@user_first, @user_last);
-
-                        SELECT * FROM Users where u_id = (SELECT max(u_id) FROM Users);
+        let checkOverlap = await pool.request()
+            .input('vehicle_id', sql.Int, vehicle_id)
+            .input('newStartDate', sql.DateTime, start_date)
+            .input('newEndDate', sql.DateTime, end_date)
+            .query(`
+                    SELECT *
+                    FROM Rentals
+                    WHERE (
+                            (r_start_date <= @newEndDate AND r_end_date >= @newStartDate) OR
+                            (r_start_date <= @newStartDate AND r_end_date >= @newEndDate) OR
+                            (r_start_date >= @newStartDate AND r_end_date <= @newEndDate)
+                        ) AND r_vehicle_id =  @vehicle_id;
                     `)
 
 
-    let addBooking = await pool.request()
-        .input('vehicle_id', sql.Int, vehicle_id)
-        .input('user_id', sql.Int, addUser.recordset[0].u_id)
-        .input('start_date', sql.DateTime, start_date)
-        .input('end_date', sql.DateTime, end_date)
-        .query(`
-                    INSERT INTO Rentals(r_vehicle_id, r_user_id, r_start_date, r_end_date)
-                    VALUES (@vehicle_id, @user_id, @start_date, @end_date);
+        if (checkOverlap.rowsAffected[0]) {
+            res.status(409).send({ error: false, message: 'Bookings exist for this vehicle at the selected time. Choose another.' })
+            return
+        }
 
-                    SELECT * FROM Rentals WHERE rental_id = (SELECT MAX(rental_id) FROM Rentals)
-                `)
+        let addUser = await pool.request()
+            .input('user_first', sql.NVarChar, user_first)
+            .input('user_last', sql.NVarChar, user_last)
+            .query(`
+                            INSERT INTO Users(u_first_name, u_last_name)
+                            VALUES (@user_first, @user_last);
+    
+                            SELECT * FROM Users where u_id = (SELECT max(u_id) FROM Users);
+                        `)
 
-    res.status(200).send({ error: false, message: 'Booking confirm !', data: addBooking.recordsets[0] })
-    return
+
+        let addBooking = await pool.request()
+            .input('vehicle_id', sql.Int, vehicle_id)
+            .input('user_id', sql.Int, addUser.recordset[0].u_id)
+            .input('start_date', sql.DateTime, start_date)
+            .input('end_date', sql.DateTime, end_date)
+            .query(`
+                        INSERT INTO Rentals(r_vehicle_id, r_user_id, r_start_date, r_end_date)
+                        VALUES (@vehicle_id, @user_id, @start_date, @end_date);
+    
+                        SELECT * FROM Rentals WHERE rental_id = (SELECT MAX(rental_id) FROM Rentals)
+                    `)
+
+        res.status(200).send({ error: false, message: 'Booking confirm !', data: addBooking.recordsets[0] })
+        return
+
+
+    }
+    catch (error) {
+        res.status(500).send({ error: true, message: error.message })
+        return
+    }
+
 
 })
 
